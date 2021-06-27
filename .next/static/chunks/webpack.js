6224631fd8ac989d851300e3d1f72297ed0777be
jhunderdog <33382water@gmail.com>
2021-06-27 09:36:42 +0900

    스타일 컴포넌트 쓰면서 리렌더링 피하기

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4adc41e95a879309117c";
+/******/ 	var hotCurrentHash = "c326dda8d3090cecc33c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,7 +1,11 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import { Form, Input, Button } from "antd";
 import Link from "next/link";
+import styled from "styled-components";
 
+const ButtonWrapper = styled.div`
+  margin-top: 10px;
+`;
 const LoginForm = () => {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
@@ -10,6 +14,12 @@ const LoginForm = () => {
   }, []);
   const onChangePassword = useCallback((e) => {
     setPassword(e.target.value);
+  }, []);
+
+  const style = useMemo(() => {
+    {
+      marginTop: 10;
+    }
   }, []);
   return (
     <Form>
@@ -29,17 +39,16 @@ const LoginForm = () => {
           required
         />
       </div>
-      <div>
+      <ButtonWrapper>
         <Button type="primary" htmlType="submit" loading={false}>
           로그인
         </Button>
-        <Link href="/signup">
-          <a>
-            <Button>회원가입</Button>
-          </a>
-        </Link>
-      </div>
-      <div></div>
+      </ButtonWrapper>
+      <Link href="/signup">
+        <a>
+          <Button>회원가입</Button>
+        </a>
+      </Link>
     </Form>
   );
 };
